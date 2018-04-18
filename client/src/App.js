d8f171d9119e6822dccdd0372c4898a038ea62c3
Alex Brant <vrknyght@gmail.com>
2018-04-17 18:19:52 -0700

    Adding Chat component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+//import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
+import Chat from './components/Chat';
+
+
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -10,11 +15,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <Chat />
       </div>
-    );
+    )
   }
 }
 

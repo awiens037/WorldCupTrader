0954cc2b59338ff5d69d4b3dec0d3d4790051617
Andy W <awiens037@gmail.com>
2018-04-24 18:41:06 -0700

    m

--- server/server.js
+++ server/server.js
@@ -48,17 +48,6 @@ app.use(bodyParser.json());
 // Serve up static
 app.use(express.static("../client/build"));
 
-
-// Set up promises with mongoose
-// mongoose.Promise = global.Promise;
-// // Connect to the Mongo DB
-// mongoose.connect(
-//     process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-//     {
-//         useMongoClient: true
-//     }
-// );
-
 // Start the API server
 app.listen(PORT, function () {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
